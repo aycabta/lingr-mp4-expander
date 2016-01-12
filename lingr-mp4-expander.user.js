1fd6c592ca3b93962ed30c11b5594a8745163746
Code Ass <aycabta@gmail.com>
2016-01-13 01:02:17 +0900

    Version 0.1.4

--- lingr-mp4-expander.user.js
+++ lingr-mp4-expander.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name       Lingr MP4 Expander
 // @namespace  http://aycabta.github.io/
-// @version    0.1.3
+// @version    0.1.4
 // @description  URL -> embedded player
 // @include    http://lingr.com/
 // @include    http://lingr.com/room/*/archives*
